0e09a5d742d9ea0a78c63554cf1cb0fd5ccb4aa6
kikemaya <enrique.mg.cd@gmail.com>
2023-08-10 21:35:50 -0600

    ES6

---
+++ ES6/funciones/funcion.js
@@ -0,0 +1,14 @@
+
+function saludar() {
+  let mensaje = "¡Hola!";
+  return mensaje;
+};
+
+console.log(saludar());
+
+// LAS FUNCIONES PIDEN PARAMETROS
+function suma(a, b) {
+}
+
+// Y RECIBEN ARGUMENTOS
+suma(7, 9)
