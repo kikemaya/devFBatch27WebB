4792445a443354f52ff377830e7f5f1159dc801f
kikemaya <enrique.mg.cd@gmail.com>
2023-08-31 20:37:11 -0600

    estructuras de datos

--- estructuras-de-datos/Array/index.js
+++ estructuras-de-datos/Array/index.js
@@ -6,12 +6,43 @@ class MyArray {
   };
   get(index) {
     return this.data[index];
-  }
+  };
   push(element) {
     this.data[this.length] = element;
     this.length++;
     return this.data;
-  }
+  };
+  pop() {
+    //TODO: VALIDAR QUE EL ARREGLO NO ESTE VACIO
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  };
+  // AÑADIR UN PRIMER ELEMENTO
+  unshift(item) {
+    // - PEDIR UN VALOR A AGREGAR
+    // - MODIFICAR LOS INDICES, DESPLAZANDOLOS HACIA ADELANTE
+    // - UNA VEZ QUE LO TENGAMOS Y QUE LOS INDICES SE HAYAN MODIFICADO
+    // - COLOCAR ESE VALOR EN EL INDICE 0
+    // - AUMENTAR LA LONGITUD DEL ARREGLO EN UNA UNIDAD
+    // - RETORNAR LA CANTIDAD NUEVA DE ELEMENTOS DEL ARREGLO
+    const lastItem = this.data[this.length - 1];
+
+    for (let i = 0; i <= this.length - 1; i++) {
+      // VALORES: undefined, 1, 2, 4
+      // INDICES: 0, 1, 2, 3
+      this.data[i + 1] = this.data[i];
+    };
+
+    this.data[this.length] = lastItem;
+    this.data[0] = item;
+
+    this.length++;
+    return this.length;
+  };
+  // TODO: CREAR EL METODO PARA REMOVER EL PRIMER ELEMENTO
+  shift() { }
 };
 
 const arr1 = new MyArray();
@@ -24,4 +55,8 @@ arr1.push(["Español", "Matematicas"]);
 arr1.push(78);
 
 console.log(arr1.get(0));
+console.log(arr1);
+
+arr1.unshift("cero");
+
 console.log(arr1);
